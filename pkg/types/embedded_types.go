f8ded3e528ab022a7eecda8a7baf754501dfc08e
Jan Chaloupka <jchaloup@redhat.com>
2017-04-01 15:04:11 +0200

    replace all built-in types with Builtin type

--- pkg/types/embedded_types.go
+++ pkg/types/embedded_types.go
@@ -1,50 +1,11 @@
 package types
 
-// For ast.BasicLit embedded types
-// token.INT, token.FLOAT, token.IMAG, token.CHAR, or token.STRING
-const IntegerType = "integer"
+const BuiltinType = "builtin"
 
-type Integer struct {
-}
+type Builtin struct{}
 
-func (i *Integer) GetType() string {
-	return IntegerType
-}
-
-const FloatType = "float"
-
-type Float struct {
-}
-
-func (f *Float) GetType() string {
-	return FloatType
-}
-
-const ImagType = "imag"
-
-type Imag struct {
-}
-
-func (i *Imag) GetType() string {
-	return ImagType
-}
-
-const CharType = "char"
-
-type Char struct {
-}
-
-func (c *Char) GetType() string {
-	return CharType
-}
-
-const StringType = "string"
-
-type String struct {
-}
-
-func (s *String) GetType() string {
-	return StringType
+func (b *Builtin) GetType() string {
+	return BuiltinType
 }
 
 const NilType = "nil"
