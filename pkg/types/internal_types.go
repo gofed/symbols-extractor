1a2dc5a5f1a9a15936ffffa1fc5a04534725d609
Jan Chaloupka <jchaloup@redhat.com>
2017-04-12 09:46:59 +0200

    introduce built-in literal data type

---
+++ pkg/types/internal_types.go
@@ -0,0 +1,41 @@
+package types
+
+const BuiltinType = "builtin"
+
+type Builtin struct{}
+
+func (b *Builtin) GetType() string {
+	return BuiltinType
+}
+
+const NilType = "nil"
+
+type Nil struct {
+}
+
+func (n *Nil) GetType() string {
+	return NilType
+}
+
+// BuiltinLiteralType is a type constant for built-in literal
+const BuiltinLiteralType = "builtinliteral"
+
+// BuiltinLiteral represents literals like true or false
+type BuiltinLiteral struct{}
+
+// GetType gets type
+func (b *BuiltinLiteral) GetType() string {
+	return BuiltinLiteralType
+}
+
+const PackageQualifierType = "packagequalifier"
+
+// PackageQualifier represents an import of a package
+type PackageQualifier struct {
+	Name string
+	Path string
+}
+
+func (p *PackageQualifier) GetType() string {
+	return PackageQualifierType
+}
